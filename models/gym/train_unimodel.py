77cbdbc3240d0d93760d1ad6382bd33e197d68cd
Yadi <yadix@andrew.cmu.edu>
2023-04-28 05:29:57 +0000

    train multiple env and epoch once execution

--- models/gym/train_unimodel.py
+++ models/gym/train_unimodel.py
@@ -1,21 +1,22 @@
+from multimodal.tem_dataloader import MultimodalDataset
+from torch.utils.data import DataLoader
+from transformers import ViTMAEForPreTraining, ViTMAEConfig
+from transformers.models.vit_mae.modeling_vit_mae import ViTMAEModel
+import torch
+import os
+from transformers.optimization import AdamW
+from transformers import get_linear_schedule_with_warmup
+import time
+import tqdm
 
 import wandb
 wandb.login() 
 # 8599fbb702cb5767e13d2ac3b1cdcc1c9b65d451
 
-from multimodal.tem_dataloader import MultimodalDataset
+TOTAL_EPOCH = 5
+NUM_WORKERS = 4
+BATCH_SIZE = 32
 trained_model_name = "unimodel_1"
-environment_name = 'AbandonedCableExposure'
-
-my_dataset = MultimodalDataset(f'/mnt/data/tartanairv2filtered/{environment_name}/Data_easy')
-
-from torch.utils.data import DataLoader
-batch_size = 32
-train_dataloader = DataLoader(my_dataset, batch_size=batch_size, num_workers=4, shuffle=True)
-
-from transformers import ViTMAEForPreTraining, ViTMAEConfig
-from transformers.models.vit_mae.modeling_vit_mae import ViTMAEModel
-import torch
 
 # create Unimodel ViT
 model_name = "facebook/vit-mae-base"
@@ -32,7 +33,6 @@ unimodel.vit = vit_model
 
 output_dir='/home/ubuntu/weights/' + trained_model_name
 
-import os
 if os.path.exists(output_dir):
     # Load the state_dict from the saved model
     state_dict = torch.load(f"{output_dir}/pytorch_model.bin")
@@ -40,60 +40,70 @@ if os.path.exists(output_dir):
     # Apply the state_dict to the custom_model
     unimodel.load_state_dict(state_dict)
 
-from transformers.optimization import AdamW
-from transformers import get_linear_schedule_with_warmup
-
-epochs = 1
-learning_rate = 0.01
-weight_decay = 0.01
-warmup_steps = 0
-total_steps = len(train_dataloader) * epochs
-optimizer = AdamW(unimodel.parameters(), lr=learning_rate, weight_decay=weight_decay)
-scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 unimodel.to(device)
 
-import time
-import tqdm
-def train(model, dataloader, optimizer, scheduler, device):
-    model.train()
-    total_loss = 0
-    total_samples = 0
-    
-    for batch in tqdm.tqdm(dataloader):
-        if trained_model_name == "unimodel_0":
-            pixel_values = batch["pixel_values"].to(device)
-        elif trained_model_name == "unimodel_1":
-            pixel_values = batch["pixel_values1"].to(device)
-        elif trained_model_name == "unimodel_2":
-            pixel_values = batch["pixel_values2"].to(device)
+env_epoch = [['AbandonedFactoryExposure', TOTAL_EPOCH],
+             ['AbandonedSchoolExposure', TOTAL_EPOCH],
+             ['AmericanDinerExposure', TOTAL_EPOCH],
+             ['AbandonedCableExposure', TOTAL_EPOCH-1]]
 
-        optimizer.zero_grad()
-        outputs = model(pixel_values, noise=None)
+train_finish = False
+env_epoch_index = 0
 
-        loss = outputs.loss
-        loss.backward()
+learning_rate = 0.01
+weight_decay = 0.01
+warmup_steps = 0
+optimizer = AdamW(unimodel.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        optimizer.step()
-        scheduler.step()
+while not all(item[1] == 0 for item in env_epoch):
+    while env_epoch[env_epoch_index][1] == 0:
+        env_epoch_index = (env_epoch_index + 1) % 4
+    # env_epoch[env_epoch_index][1] now must not 0
+    environment_name = env_epoch[env_epoch_index][0] # environment_name = 'AbandonedCableExposure'
+    env_epoch[env_epoch_index][1] -= 1
+    env_epoch_index = (env_epoch_index + 1) % 4
 
-        total_loss += loss.item() * pixel_values.size(0)
-        total_samples += pixel_values.size(0)
+    my_dataset = MultimodalDataset(f'/mnt/data/tartanairv2filtered/{environment_name}/Data_easy')
+    train_dataloader = DataLoader(my_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
+    total_steps = len(train_dataloader)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
 
-        if total_samples % 32 == 0:
+    def train(model, dataloader, optimizer, scheduler, device):
+        model.train()
+        total_loss = 0
+        total_samples = 0
+        
+        for batch in tqdm.tqdm(dataloader):
+            if trained_model_name == "unimodel_0":
+                pixel_values = batch["pixel_values"].to(device)
+            elif trained_model_name == "unimodel_1":
+                pixel_values = batch["pixel_values1"].to(device)
+            elif trained_model_name == "unimodel_2":
+                pixel_values = batch["pixel_values2"].to(device)
+
+            optimizer.zero_grad()
+            outputs = model(pixel_values, noise=None)
+
+            loss = outputs.loss
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+            scheduler.step()
+
+            total_loss += loss.item() * pixel_values.size(0)
+            total_samples += pixel_values.size(0)
+
             wandb.log({'loss': loss.item()} )
-            # print(loss.item())
 
-    return total_loss / total_samples
+        return total_loss / total_samples
 
-wandb.init(project="11777",name=trained_model_name+"_"+environment_name+time.strftime("%Y%m%d-%H%M%S"))
-for epoch in range(epochs):
+    wandb.init(project="11777",name=trained_model_name+"_"+environment_name+time.strftime("%Y%m%d-%H%M%S"))
     train_loss = train(unimodel, train_dataloader, optimizer, scheduler, device)
-    print(f"Epoch: {epoch + 1}, Loss: {train_loss:.4f}")
+    print(f"{environment_name} Loss: {train_loss:.4f}")
     unimodel.save_pretrained(output_dir)
-wandb.finish()
+    wandb.finish()
 
 unimodel.save_pretrained(output_dir)
