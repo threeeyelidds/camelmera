3056e24edda10144d7e8950d04958f46bad8ab7b
_quanting_aws <yadix@andrew.cmu.edu>
2023-04-29 04:39:38 +0000

    train multiple env and epoch once execution

--- models/gym/train_mutil_model_encoder.py
+++ models/gym/train_mutil_model_encoder.py
@@ -1,4 +1,5 @@
 
+import os
 from multimodal.tem_dataloader import MultimodalDataset
 import wandb
 from multimodal.custom_models.CustomViT import CustomViT
@@ -14,96 +15,93 @@ from transformers import get_linear_schedule_with_warmup
 wandb.login()
 # # 8599fbb702cb5767e13d2ac3b1cdcc1c9b65d451
 
-environment_name = 'AmericanDinerExposure'
-
-my_dataset = MultimodalDataset(
-    f'/media/tyz/3B6FFE7354FF3296/11_777/tartanairv2filtered/{environment_name}/Data_easy')
-
-batch_size = 32
-train_dataloader = DataLoader(
-    my_dataset, batch_size=batch_size, num_workers=4, shuffle=True)
-
-output_dir = '/home/tyz/Desktop/11_777/camelmera/weights'
+TOTAL_EPOCH = 2
+NUM_WORKERS = 16
+BATCH_SIZE = 32
+trained_model_name = "multimodal"
 
 # Initialize a new CustomViTMAE model
 model_name = "facebook/vit-mae-base"
-
 vit_config = ViTMAEConfig.from_pretrained(model_name)
 vit_config.output_hidden_states = True
 vit_model = CustomViT.from_pretrained(model_name, config=vit_config)
-
-
 model_name = "facebook/vit-mae-base"
-
 config = ViTMAEConfig.from_pretrained(model_name)
 config.output_hidden_states = True
-
 # load from pretrained model and replace the original encoder with custom encoder
 custom_model = CustomViTMAE.from_pretrained(
     "facebook/vit-mae-base", config=config)
 custom_model.vit = vit_model
 
-# Load the state_dict from the saved model
-state_dict = torch.load(f"{output_dir}/pytorch_model.bin")
+output_dir = '/home/ubuntu/weights/' + trained_model_name
 
-# Apply the state_dict to the custom_model
-custom_model.load_state_dict(state_dict)
-
-
-def train(model, dataloader, optimizer, scheduler, device):
-    model.train()
-    total_loss = 0
-    total_samples = 0
-
-    for batch in dataloader:
-        pixel_values = batch["pixel_values"].to(device)
-        pixel_values1 = batch["pixel_values1"].to(device)
-        pixel_values2 = batch["pixel_values2"].to(device)
-
-        optimizer.zero_grad()
-        outputs = model(pixel_values, pixel_values1, pixel_values2, noise=None)
-
-        loss = outputs.loss
-        loss.backward()
-
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        optimizer.step()
-        scheduler.step()
-
-        total_loss += loss.item() * pixel_values.size(0)
-        total_samples += pixel_values.size(0)
-
-        if total_samples % (batch_size * 1) == 0:  # log every 1 batches
-            wandb.log({'loss': loss.item()})
-            print(loss.item())
-
-    return total_loss / total_samples
-
-
-epochs = 1
-learning_rate = 0.01
-weight_decay = 0.01
-warmup_steps = 0
-total_steps = len(train_dataloader) * epochs
-optimizer = AdamW(custom_model.parameters(),
-                  lr=learning_rate, weight_decay=weight_decay)
-scheduler = get_linear_schedule_with_warmup(
-    optimizer, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
+if os.path.exists(output_dir):
+    # Load the state_dict from the saved model
+    state_dict = torch.load(f"{output_dir}/pytorch_model.bin")
+    # Apply the state_dict to the custom_model
+    custom_model.load_state_dict(state_dict)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 custom_model.to(device)
 
+env_epoch = [['AbandonedCableExposure', TOTAL_EPOCH],
+             ['AbandonedFactoryExposure', TOTAL_EPOCH],
+             ['AbandonedSchoolExposure', TOTAL_EPOCH],
+             ['AmericanDinerExposure', TOTAL_EPOCH]]
 
-wandb.init(project="11777", name=environment_name +
-           time.strftime("%Y%m%d-%H%M%S"))
-for epoch in range(epochs):
-    train_loss = train(custom_model, train_dataloader,
-                       optimizer, scheduler, device)
-    print(f"Epoch: {epoch + 1}, Loss: {train_loss:.4f}")
-    output_dir = '/home/tyz/Desktop/11_777/camelmera/weights'
+train_finish = False
+env_epoch_index = 0
+
+learning_rate = 0.01
+weight_decay = 0.01
+warmup_steps = 0
+optimizer = AdamW(custom_model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+while not all(item[1] == 0 for item in env_epoch):
+    while env_epoch[env_epoch_index][1] == 0:
+        env_epoch_index = (env_epoch_index + 1) % 4
+    # env_epoch[env_epoch_index][1] now must not 0
+    environment_name = env_epoch[env_epoch_index][0] # environment_name = 'AbandonedCableExposure'
+    env_epoch[env_epoch_index][1] -= 1
+    env_epoch_index = (env_epoch_index + 1) % 4
+
+    my_dataset = MultimodalDataset(f'/mnt/data/tartanairv2filtered/{environment_name}/Data_easy')
+    train_dataloader = DataLoader(my_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
+    total_steps = len(train_dataloader)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
+
+    def train(model, dataloader, optimizer, scheduler, device):
+        model.train()
+        total_loss = 0
+        total_samples = 0
+
+        for batch in dataloader:
+            pixel_values = batch["pixel_values"].to(device)
+            pixel_values1 = batch["pixel_values1"].to(device)
+            pixel_values2 = batch["pixel_values2"].to(device)
+
+            optimizer.zero_grad()
+            outputs = model(pixel_values, pixel_values1, pixel_values2, noise=None)
+
+            loss = outputs.loss
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+            scheduler.step()
+
+            total_loss += loss.item() * pixel_values.size(0)
+            total_samples += pixel_values.size(0)
+
+            wandb.log({'loss': loss.item()} )
+
+        return total_loss / total_samples
+
+    wandb.init(project="11777",name=trained_model_name+"_"+environment_name+time.strftime("%Y%m%d-%H%M%S"))
+    train_loss = train(custom_model, train_dataloader, optimizer, scheduler, device)
+    print(f"{environment_name} Loss: {train_loss:.4f}")
     custom_model.save_pretrained(output_dir)
-wandb.finish()
+    wandb.finish()
 
-output_dir = '/home/tyz/Desktop/11_777/camelmera/weights'
 custom_model.save_pretrained(output_dir)
