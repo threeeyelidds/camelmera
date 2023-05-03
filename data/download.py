712b2a56fd32770fe07990e70704899ceff7eebb
threeeyelidds <youqunew@gmail.com>
2023-05-03 23:40:27 +0000

    download factory

--- data/download.py
+++ data/download.py
@@ -4,7 +4,7 @@ import os
 
 
 # default setting would download all trajacories to /data/tartanairv2filtered/
-destination_folder_base = '/home/ubuntu/data'
+destination_folder_base = '/mnt/temp_mount'
 
 # Define your Azure Blob Storage connection string and container name
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=tartanairv2;AccountKey=PH1q1TB4fHqGFnvfivj9jPrvABz2ESX1OKkrA4+8G3qoHJpIPDmDok4d2uzDNF7RVR9A4cE7Y00n5nsCrc+edA==;EndpointSuffix=core.windows.net'
@@ -26,13 +26,15 @@ def download():
     path_combinations = ['depth_lcam_fish', 'image_lcam_fish', 'lidar', 'pose_lcam_front.txt']
 
     blob_list = blob_container_client.list_blobs()
-
     for blob in blob_list:
         path_lists = blob.name.split('/')
+        if path_lists[0] != 'AbandonedFactoryExposure':
+            continue 
+        if int(path_lists[2][-1])<5:
+            # print(int(path_lists[2][-1]))
+            continue
         if len(path_lists) > 3 and path_lists[1] == "Data_easy" and path_lists[3] in path_combinations:
-            # print(blob.name) #e.g., AbandonedCableExposure/Data_easy/P000/depth_lcam_fish/000860_lcam_fish_depth.png
-            if 'AbandonedFactoryExposure' not in blob.name:
-                continue
+            print(blob.name) #e.g., AbandonedCableExposure/Data_easy/P000/depth_lcam_fish/000860_lcam_fish_depth.png
             blob_client = blob_container_client.get_blob_client(blob.name)
 
             filename = blob.name.split("/")[-1]
